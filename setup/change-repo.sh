62b290756c95568521f8e93a2d4210b12fe8accb
Adrian Knorr <adrian.knorr@consol.de>
2024-03-26 10:16:32 +0100

    Change repo

--- setup/change-repo.sh
+++ setup/change-repo.sh
@@ -5,6 +5,6 @@ REPOPATH=$(dirname $MYPATH)
 
 GITREPO=$(git remote -v | awk -F '[:/ ]' '/origin.*push/ {print $(NF-2) "/" $(NF-1)}')
 
-# changing     repoURL: https://github.com/cwilkers/kubevirt-gitops
+# changing     repoURL: https://gitlab.consol.de/openshift/kubevirt-gitops.git
 
 sed -i "s@repoURL: https://github.com/.*@repoURL: https://github.com/${GITREPO}@" ${REPOPATH}/*/application.yaml
